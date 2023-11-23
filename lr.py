babbfbc102e1a879605a5d0e4beb19c6e4876855
Erick Morales <54952779+erijmo@users.noreply.github.com>
2023-11-22 23:37:57 -0500

    Update lr.py

--- lr.py
+++ lr.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 
-github_csv_url = 'https://raw.githubusercontent.com/erijmo/3690/main/CAR%20DETAILS%20FROM%20CAR%20DEKHO.csv'
-
-df = pd.read_csv(github_csv_url)
+url = 'https://raw.githubusercontent.com/erijmo/3690/main/CAR%20DETAILS%20FROM%20CAR%20DEKHO.csv'
+df = pd.read_csv(url)
 
 X = df['year'].values
 y = df['selling_price'].values
@@ -13,15 +12,12 @@ mean_y = np.mean(y)
 
 numerator = np.sum((X - mean_X) * (y - mean_y))
 denominator = np.sum((X - mean_X) ** 2)
+
 beta = numerator / denominator
 alpha = mean_y - beta * mean_X
 
 X_test = np.arange(1992, 2021)
 y_pred = alpha + beta * X_test
-
-
-print(f'Slope (beta): {beta}')
-print(f'Intercept (alpha): {alpha}')
 
 for year, pred in zip(X_test, y_pred):
     print(f'Prediction for year {year}: {pred}')
