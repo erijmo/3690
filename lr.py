bc0d0deb1438128ee32bd1c306e79a090cd82e53
Erick Morales <54952779+erijmo@users.noreply.github.com>
2023-11-22 23:31:10 -0500

    Add files via upload

---
+++ lr.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+
+file_path = r'C:\Users\Erick\Downloads\CAR DETAILS FROM CAR DEKHO.csv'
+df = pd.read_csv(file_path)
+
+
+X = df['year'].values
+y = df['selling_price'].values
+
+
+mean_X = np.mean(X)
+mean_y = np.mean(y)
+
+
+numerator = np.sum((X - mean_X) * (y - mean_y))
+denominator = np.sum((X - mean_X) ** 2)
+
+
+beta = numerator / denominator
+alpha = mean_y - beta * mean_X
+
+
+X_test = np.arange(1992, 2021)
+y_pred = alpha + beta * X_test
+
+
+print(f'Slope (beta): {beta}')
+print(f'Intercept (alpha): {alpha}')
+
+
+for year, pred in zip(X_test, y_pred):
+    print(f'Prediction for year {year}: {pred}')
